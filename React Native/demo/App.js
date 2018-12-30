490566702035b1a57b0684da16724167186d242c
Krishna <apple@MacBook-Pro.local>
2018-12-30 10:29:01 +0530

    Created Expo projects.

--- React Native/demo/App.js
+++ React Native/demo/App.js
@@ -1,49 +1,29 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import { View, Text, TextInput, Button, Alert} from 'react-native';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
+export default class Login extends React.Component{
+  render(){
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native Demo!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View>
+        <Text>Welcome to Login</Text>
+        <View style={{flex: 1, flexDirection: 'row', alignItems: 'stretch'}}>
+          <View style={{width: 150, height: 50, backgroundColor: 'powderblue'}}>
+            <Text>User Name</Text>
+          </View>
+          <View style={{width:150, height: 50, backgroundColor: 'skyblue'}}>
+            <TextInput
+              style={{height: 40, width: 150}}
+              placeholder="UserName"
+            />
+          </View>
+        </View>
+        <Button
+          onPress={() => {
+            Alert.alert('You tapped the button!');
+          }}
+          title="Login "
+        />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
